b64def1025728c57c134f5b5fcedf7cc59bf4ff9
Yama-Tomo <mazda13bturbo@gmail.com>
2019-10-03 14:46:35 +0900

    chore: 不要になった関数の削除

--- src/containers/factory.tsx
+++ src/containers/factory.tsx
@@ -1,17 +1,5 @@
 import React from 'react';
 
-export const containerFactory = function<T>(defaultRender: React.FC<T>, preparePropsFn: () => T) {
-  return (ownProps?: { render?: React.FC<T> }) => {
-    const props = preparePropsFn();
-
-    const PresentationalComponent = ownProps && ownProps.render
-      ? ownProps.render
-      : defaultRender;
-
-    return <PresentationalComponent {...props} />;
-  }
-};
-
 export const createRenderFunction = function<T>(defaultRender: React.FC<T>, customRender?: React.FC<T>) {
   const PresentationalComponent = customRender
     ? customRender
