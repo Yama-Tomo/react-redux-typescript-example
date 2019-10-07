740b4f8278c25ce1c1e2600206721981f93fa630
Yama-Tomo <mazda13bturbo@gmail.com>
2019-10-07 11:38:01 +0900

    feat: react-routerを使って画面を分割した

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,13 +2,18 @@ import { combineReducers } from 'redux';
 import { configureStore, getDefaultMiddleware } from 'redux-starter-kit';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { createBrowserHistory } from 'history';
+import { connectRouter } from 'connected-react-router';
 import { all } from 'redux-saga/effects';
 
 import * as nameModule from './name';
 import * as counterModule from './counter';
 import * as searchModule from './search';
 
+export const history = createBrowserHistory();
+
 const rootReducer = combineReducers({
+  router: connectRouter(history),
   name: nameModule.default.reducer,
   counter: counterModule.default.reducer,
   search: searchModule.default.reducer,
