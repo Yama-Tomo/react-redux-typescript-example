7473a5ecc41eedab37ea4fae39f28c423d86507a
Yama-Tomo <mazda13bturbo@gmail.com>
2019-10-07 17:51:52 +0900

    feat: layoutをmaterial-uiを使って書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,117 @@
 import React from 'react';
-import './App.css';
-import styled, { ThemeProvider } from 'styled-components';
+import { useSelector } from 'react-redux';
+import { RootState } from './state/store';
 import { Route, Switch } from 'react-router';
+
+import './App.css';
+import logo from './logo.svg';
+import styled, { CSSObject, ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { AppBar,Typography,CssBaseline, Toolbar, Drawer, List, ListItem, ListItemText, Theme } from '@material-ui/core';
+import { ThemeProvider as MuiThemeProvider, StylesProvider } from '@material-ui/styles';
+import * as Themes from './theme';
+import { Link } from 'react-router-dom';
+
 import NameContainer from './containers/name';
 import CounterContainer from './containers/counter';
 import SearchContainer from './containers/search';
-import { Link } from 'react-router-dom';
 
-const H2 = styled.h2`
-  background-color: #454545;
-  color: #f08282;
+const Container = styled.div`
+  display: flex;
 `;
 
-const Separator = styled.hr`
+const StyledAppBar = styled(AppBar)`
+  z-index: ${(props: { theme: Theme }) => props.theme.zIndex.drawer + 1};
+`;
+
+const drawerWidth = 200;
+const StyledDrawer = styled(Drawer)`
+  width: ${drawerWidth}px;
+  flex-shrink: 0;
+  
+  > div {
+    width: ${drawerWidth}px;
+  }
+`;
+
+const Main = styled.main`
+  padding: ${(props: { theme: Theme }) => props.theme.spacing(2)}px;
+  flex-grow: 1;
+  min-height: 100vh;
+`;
+
+const Home = styled.div`
+  background-color: ${(props: { theme: Theme }) => props.theme.palette.background.paper};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   width: 100%;
+  height: 100%;
 `;
 
-const NavBars = styled.div`
-  & a { color: white; }
-`;
+const ToolbarSpacing = styled.div((props: { theme: Theme }) => props.theme.mixins.toolbar as CSSObject);
 
 const App: React.FC = () => {
+  const currentPath = useSelector((state: RootState) => state.router.location.pathname);
+  const isHome = currentPath === '/';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <H2>react sample</H2>
-        <NavBars>
-          <p><Link to="/">Home</Link></p>
-          <p><Link to="/basic-input">Basic redux usage</Link></p>
-          <p><Link to="/counter">Counter</Link></p>
-          <p><Link to="/autocomplete-with-redux-saga">Redux saga - autocomplete</Link></p>
-        </NavBars>
-        <Separator />
-        <Switch>
-          <Route exact path="/">
-            <></>
-          </Route>
-          <Route path="/basic-input">
-            <ThemeProvider theme={{padding: 5}}>
-              <NameContainer />
-            </ThemeProvider>
-          </Route>
-          <Route path="/counter" >
-            <CounterContainer />
-          </Route>
-          <Route path="/autocomplete-with-redux-saga" >
-            <SearchContainer />
-          </Route>
-        </Switch>
-      </header>
-    </div>
+    <StylesProvider injectFirst>
+      <MuiThemeProvider theme={isHome ? Themes.dark : Themes.main}>
+        <StyledThemeProvider theme={isHome ? Themes.dark : Themes.main}>
+          <Container>
+            <CssBaseline />
+            <StyledAppBar position="fixed">
+              <Toolbar>
+                <Typography variant="h6" noWrap>React redux typescript example</Typography>
+              </Toolbar>
+            </StyledAppBar>
+            <MuiThemeProvider theme={Themes.dark}>
+              <StyledThemeProvider theme={Themes.dark}>
+                <StyledDrawer variant="permanent">
+                  <ToolbarSpacing />
+                  <List>
+                    <ListItem selected={currentPath === '/'} button component={Link} to='/'>
+                      <ListItemText primary={'home'} />
+                    </ListItem>
+                    <ListItem selected={currentPath === '/basic-input'} button component={Link} to='/basic-input'>
+                      <ListItemText primary={'basic redux usage'} />
+                    </ListItem>
+                    <ListItem selected={currentPath === '/counter'} button component={Link} to='/counter'>
+                      <ListItemText primary={'counter'} />
+                    </ListItem>
+                    <ListItem selected={currentPath === '/autocomplete-with-redux-saga'} button component={Link} to='/autocomplete-with-redux-saga'>
+                      <ListItemText primary={'redux saga'} secondary={'autocomplete'} />
+                    </ListItem>
+                  </List>
+                </StyledDrawer>
+              </StyledThemeProvider>
+            </MuiThemeProvider>
+            <Main>
+              {!isHome && <ToolbarSpacing />}
+              <Switch>
+                <Route exact path="/">
+                  <Home>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p style={{'fontSize': '1.3rem'}}>Welcome to React example</p>
+                  </Home>
+                </Route>
+                <Route path="/basic-input">
+                  <NameContainer />
+                </Route>
+                <Route path="/counter" >
+                  <CounterContainer />
+                </Route>
+                <Route path="/autocomplete-with-redux-saga" >
+                  <SearchContainer />
+                </Route>
+              </Switch>
+            </Main>
+          </Container>
+        </StyledThemeProvider>
+      </MuiThemeProvider>
+    </StylesProvider>
   );
-}
+};
 
 export default App;
